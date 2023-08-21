7affed2f3c886738439699ca6442e9c9673d5c69
marsaturn <maaronjohnsaturnino@gmail.com>
2023-08-21 16:26:10 -0700

    s

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import { CurrentUserLoader } from './CurrentUserLoader';
 import { UserLoader } from './UserLoader';
 import { ResourceLoader } from './ResourceLoader';
+import { DataSource } from './DataSource';
 import { ProductInfo } from './ProductInfo';
 import { UserInfo } from './UserInfo';
+import axios from 'axios';
+
+const getServerData = url => async () => {
+	const response = await axios.get(url);
+	return response.data;
+}
+
+const getLocalStorageData = key => () => {
+	return localStorage.getItem(key);
+}
+
+const Text = ({ message }) => <h1>{message}</h1>
 
 function App() {
 	return (
 		<>
-		<ResourceLoader resourceUrl="/users/123" resourceName="user">
+		<DataSource getDataFunc={getServerData('/users/123')}	
+					resourceName="user">
 			<UserInfo />
-		</ResourceLoader>
-		<ResourceLoader resourceUrl="/products/1234" resourceName="product">
-			<ProductInfo />
-		</ResourceLoader>
+		</DataSource>
+		<DataSource getDataFunc={getLocalStorageData('message')}
+					resourceName="message">
+			<Text />
+		</DataSource>
 		</>
 	);
 }
